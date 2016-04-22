70aa5ba4689268e67bb27b64cba0da39f40bad22
Simon Karlsson <simonkarlsson@msn.com>
2016-04-22 15:35:19 +0200

    Collect symbols in declarations

--- src/main/scala/slacc/analyzer/NameAnalysis.scala
+++ src/main/scala/slacc/analyzer/NameAnalysis.scala
@@ -11,6 +11,21 @@ object NameAnalysis extends Pipeline[Program, Program] {
     import ctx.reporter._
 
     // Step 1: Collect symbols in declarations
+    for (classDecl <- prog.classes) {
+        classDecl.setSymbol(new ClassSymbol(classDecl.id.value))
+
+        for (classVar <- classDecl.vars) {
+          classVar.setSymbol(new VariableSymbol(classVar.id.value))
+        }
+
+        for (method <- classDecl.methods) {
+          method.setSymbol(new MethodSymbol(method.id.value, classDecl.getSymbol))
+
+          for (methodVar <- method.vars) {
+            methodVar.setSymbol(new VariableSymbol(methodVar.id.value))
+          }
+        }
+    }
     // Step 2: Attach symbols to identifiers (except method calls) in method bodies
     // (Step 3:) Print tree with symbol ids for debugging
 
