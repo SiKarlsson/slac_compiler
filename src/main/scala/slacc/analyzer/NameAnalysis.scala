333ff458733923219c034ae0085c3117e2a24d21
Marcus Nordberg <nordberg@github.com>
2016-04-27 14:53:43 +0200

    Handle previously added classes

--- src/main/scala/slacc/analyzer/NameAnalysis.scala
+++ src/main/scala/slacc/analyzer/NameAnalysis.scala
@@ -10,22 +10,52 @@ object NameAnalysis extends Pipeline[Program, Program] {
   def run(ctx: Context)(prog: Program): Program = {
     import ctx.reporter._
 
+    var glob = new GlobalScope()
+
+    prog.main.setSymbol(new ClassSymbol("Main"))
+    println("MM--".concat(prog.main.getSymbol.id.toString))
+
     // Step 1: Collect symbols in declarations
     for (classDecl <- prog.classes) {
-        classDecl.setSymbol(new ClassSymbol(classDecl.id.value))
+      val classId = classDecl.id.value
+      glob.lookupClass(classId) match {
+        case Some(s) => {
+          println(classId, " already defined")
+        }
+        case None => {
+          classDecl.setSymbol(new ClassSymbol(classId))
+          glob.addClass(classId, classDecl.getSymbol)
+        }
+      }
+      println("C--".concat(classDecl.getSymbol.id.toString))
 
-        for (classVar <- classDecl.vars) {
-          classVar.setSymbol(new VariableSymbol(classVar.id.value))
+      for (classVar <- classDecl.vars) {
+        classVar.setSymbol(new VariableSymbol(classVar.id.value))
+        println("CV--".concat(classVar.getSymbol.id.toString))
+      }
+
+      for (method <- classDecl.methods) {
+        method.setSymbol(new MethodSymbol(method.id.value, classDecl.getSymbol))
+        println("M--".concat(method.getSymbol.id.toString))
+
+        for (arg <- method.args) {
+          arg.setSymbol(new VariableSymbol(arg.id.value))
+          println("MP--".concat(arg.getSymbol.id.toString))
         }
 
-        for (method <- classDecl.methods) {
-          method.setSymbol(new MethodSymbol(method.id.value, classDecl.getSymbol))
-
-          for (methodVar <- method.vars) {
-            methodVar.setSymbol(new VariableSymbol(methodVar.id.value))
-          }
+        for (methodVar <- method.vars) {
+          methodVar.setSymbol(new VariableSymbol(methodVar.id.value))
+          println("MV--".concat(methodVar.getSymbol.id.toString))
         }
+
+        for (expr <- method.exprs) {
+
+        }
+
+        // TODO: Handle retexpr of method
+      }
     }
+
     // Step 2: Attach symbols to identifiers (except method calls) in method bodies
     // (Step 3:) Print tree with symbol ids for debugging
 
