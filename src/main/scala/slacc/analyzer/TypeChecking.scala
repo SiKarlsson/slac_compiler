fd5ceeed082872097d2523fada56e6e05f517fd2
Marcus Nordberg <nordberg@github.com>
2016-05-06 15:22:56 +0200

    Typechecking for Or, True, False, IntLit, StrLit, Identifier

--- src/main/scala/slacc/analyzer/TypeChecking.scala
+++ src/main/scala/slacc/analyzer/TypeChecking.scala
@@ -6,6 +6,7 @@ import ast.Trees._
 import Symbols._
 import Types._
 import utils._
+import NameAnalysis.getTypeOfTypeTree
 
 object TypeChecking extends Pipeline[Program, Program] {
 
@@ -14,11 +15,36 @@ object TypeChecking extends Pipeline[Program, Program] {
   def run(ctx: Context)(prog: Program): Program = {
     import ctx.reporter._
 
+    for (classDecl <- prog.classes) {
+      for (methodDecl <- classDecl.methods) {
+        tcExpr(methodDecl.retExpr, getTypeOfTypeTree(methodDecl.retType))
+      }
+    }
+
     def tcExpr(expr: ExprTree, expected: Type*): Type = {
       val tpe: Type = expr match {
         case And(lhs, rhs) => {
           tcExpr(lhs, TBoolean)
           tcExpr(rhs, TBoolean)
+        }
+        case Or(lhs, rhs) => {
+          tcExpr(lhs, TBoolean)
+          tcExpr(rhs, TBoolean)
+        }
+        case True() => {
+          TBoolean
+        }
+        case False() => {
+          TBoolean
+        }
+        case IntLit() => {
+          TInt
+        }
+        case StrLit() => {
+          TString
+        }
+        case Identifier(id) => {
+          expr.getType
         }
         case _ => { sys.error("No typechecking for " + expr)}
       }
