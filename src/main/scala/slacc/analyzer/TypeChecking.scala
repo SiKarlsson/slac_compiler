0043a27d9802e458030e129ddd2abd2549520754
Simon Karlsson <simonkarlsson@msn.com>
2016-05-06 15:23:47 +0200

    Cases in type checking

--- src/main/scala/slacc/analyzer/TypeChecking.scala
+++ src/main/scala/slacc/analyzer/TypeChecking.scala
@@ -20,6 +20,26 @@ object TypeChecking extends Pipeline[Program, Program] {
           tcExpr(lhs, TBoolean)
           tcExpr(rhs, TBoolean)
         }
+        case Plus(lhs: ExprTree, rhs: ExprTree) => ???
+        case Minus(lhs: ExprTree, rhs: ExprTree) => ???
+        case Times(lhs: ExprTree, rhs: ExprTree) => {
+          tcExpr(lhs, TInt)
+          tcExpr(rhs, TInt)
+        }
+        case Div(lhs: ExprTree, rhs: ExprTree) => {
+          tcExpr(lhs, TInt)
+          tcExpr(rhs, TInt)
+        }
+        case LessThan(lhs: ExprTree, rhs: ExprTree) => ???
+        case Equals(lhs: ExprTree, rhs: ExprTree) => ???
+        case ArrayRead(arr: ExprTree, index: ExprTree) => {
+          tcExpr(arr, TIntArray)
+          tcExpr(index, TInt)
+        }
+        case ArrayLength(arr: ExprTree) => {
+          tcExpr(arr, TIntArray)
+        }
+        case MethodCall(obj: ExprTree, meth: Identifier, args: List[ExprTree]) => ???
         case _ => { sys.error("No typechecking for " + expr)}
       }
 
