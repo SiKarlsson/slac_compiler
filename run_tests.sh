7e5493a64a1ddc5de84faca0873e1dba761f7c93
Marcus Nordberg <nordberg@github.com>
2016-05-11 17:14:00 +0200

    Add test script to try all valid programs

---
+++ run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ERROR=()
+for f in ./testprograms_public_typer/lab5/valid/*.slac
+do
+	echo "--------------------------"
+	echo "COMPILING $f"
+	sbt "run $f"
+	if [ $? == 1 ] 
+		then
+		ERROR+=($f)
+	fi
+done
+
+echo "--------------------------------"
+echo "Files that crashed the compiler: "
+printf '%s\n' "${ERROR[@]}"
