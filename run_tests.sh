99bac18a65599c2e4a8fc3118e60a96e64eef1d4
Marcus Nordberg <nordberg@github.com>
2016-05-23 11:52:49 +0200

    Test if program runs as well

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 ERROR=()
+COMPILES=()
+RUNS=()
 for f in ./testprograms_public_typer/lab5/valid/*.slac
 do
 	echo "--------------------------"
 	echo "COMPILING $f"
-	sbt "run $f --symid"
-	if [ $? == 1 ] 
+	sbt "run $f -d test_dir/$(basename $f)"
+	if [ $? == 1 ]
 		then
-		ERROR+=($f)
+			ERROR+=($f)
+	else
+			COMPILES+=($f)
+			echo "RUNNING test_dir/$(basename $f)"
+			cd "test_dir/$(basename $f)";
+			java Main
+			cd ../..
+			if [ $? == 0 ]
+				then
+					RUNS+=($f)
+			fi
 	fi
 done
 
 echo "--------------------------------"
 echo "Files that crashed the compiler: "
 printf '%s\n' "${ERROR[@]}"
+echo "Files that compile (not guaranteed to run): "
+printf '%s\n' "${COMPILES[@]}"
+echo "Files that run: "
+printf '%s\n' "${RUNS[@]}"
