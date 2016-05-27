c4a79bfb54b28c8176c57802152eda8d0fe83c5a
Marcus Nordberg <nordberg@github.com>
2016-05-27 14:15:50 +0200

    Properly check if file runs or not

--- run_tests.sh
+++ run_tests.sh
@@ -11,22 +11,23 @@ do
 		then
 			ERROR+=($f)
 	else
-			COMPILES+=($f)
 			echo "RUNNING test_dir/$(basename $f)"
 			cd "test_dir/$(basename $f)";
 			java Main
-			cd ../..
 			if [ $? == 0 ]
 				then
 					RUNS+=($f)
+				else
+					COMPILES+=($f)
 			fi
+			cd ../..
 	fi
 done
 
 echo "--------------------------------"
 echo "Files that crashed the compiler: "
 printf '%s\n' "${ERROR[@]}"
-echo "Files that compile (not guaranteed to run): "
+echo "Files that compile (but doesn't run): "
 printf '%s\n' "${COMPILES[@]}"
 echo "Files that run: "
 printf '%s\n' "${RUNS[@]}"
